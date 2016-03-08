bf365f246434df2468f1521a17947a32ee60f151
Petros Amiridis <petros@amiridis.net>
2016-03-08 17:34:27 +0200

    Add exceptions for certain methods

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,4 +1,5 @@
 class DishesController < ApplicationController
+  before_filter :require_hungry_person, :except => [:new, :create, :index]
   before_action :set_dish, only: [:show, :edit, :update, :destroy]
 
   # GET /dishes
