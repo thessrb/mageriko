dabdd45361b5a6eba6d17e6a1af0d989874322d4
Petros Amiridis <petros@amiridis.net>
2016-03-08 16:24:44 +0200

    Migrate HungryPerson resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308104425) do
+ActiveRecord::Schema.define(version: 20160308141532) do
 
   create_table "dishes", force: :cascade do |t|
     t.string   "name"
     t.string   "image_url"
     t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "hungry_people", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
