71cdff033852ef657f2b0d2395a6492ef3857310
Petros Amiridis <petros@amiridis.net>
2016-03-08 16:38:58 +0200

    Add code to handle current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+  before_filter :require_user
+
+private
+
+  def current_user
+    @current_user ||= User.limit(1).where('id = ?', session[:user_id])
+  end
+
+  def require_user
+    unless current_user
+      flash[:notice] = "Πρέπει να επιλέξετε πειναλέο παρακαλώ"
+      redirect_to hungry_people_url
+      return false
+    end
+  end
+
 end
